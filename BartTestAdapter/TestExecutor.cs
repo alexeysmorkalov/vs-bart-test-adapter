6cb35243132f47e0b6657ac743226171ec8c6bac
Alexey Smorkalov <magen-ta@yandex.ru>
2016-05-17 00:34:40 +0300

    Some cleanup, reorder.

---
+++ BartTestAdapter/TestExecutor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+namespace BartTestAdapter
+{
+    
+    public class TestExecutor: ITestExecutor
+    {
+        public void Cancel()
+        {
+
+        }
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+
+        }
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+
+        }
+
+    }
+}
---
+++ BartUnitTestAdapter/BartTestAdapterTests/TestExecutorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using BartTestAdapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BartTestAdapter.Tests
+{
+    [TestFixture()]
+    public class TestExecutorTests
+    {
+        [Test()]
+        public void CancelTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void RunTestsTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void RunTestsTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
